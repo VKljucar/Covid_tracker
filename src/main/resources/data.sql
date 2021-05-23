177544b593c4956241e850920045dfc9f89f2fcd
vkljucar <valentino.kljucar@gmail.com>
2021-05-23 17:50:06 +0200

    sql data changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO billionaires (first_name, last_name, career)
-VALUES ('Aliko', 'Dangote', 'Industrialist'),
-       ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-       ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+INSERT INTO users (first_name, last_name, username, password, role)
+VALUES ('Valentino', 'Kljucar', 'vkljucar', 'pass1', 'A'),
+       ('Mirko', 'Rimac', 'mrimac', 'pass2', 'A'),
+       ('UserFirstname', 'UserLastname', 'user', 'pass3', 'U');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,9 @@
-CREATE TABLE billionaires
+CREATE TABLE users
 (
     id         INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(250) NOT NULL,
     last_name  VARCHAR(250) NOT NULL,
-    career     VARCHAR(250) DEFAULT NULL
+    username   VARCHAR(250) NOT NULL,
+    password   VARCHAR(250) NOT NULL,
+    role       VARCHAR(1) DEFAULT NULL
 );
