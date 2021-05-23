eaee5ca1031853bf2e4551d4f1b51ac8e1aae27b
vkljucar <valentino.kljucar@gmail.com>
2021-05-23 21:19:39 +0200

    local file database added

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (first_name, last_name, username, password, role)
-VALUES ('Valentino', 'Kljucar', 'vkljucar', 'pass1', 'A'),
-       ('Mirko', 'Rimac', 'mrimac', 'pass2', 'A'),
-       ('UserFirstname', 'UserLastname', 'user', 'pass3', 'U');
--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users
-(
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(250) NOT NULL,
-    last_name  VARCHAR(250) NOT NULL,
-    username   VARCHAR(250) NOT NULL,
-    password   VARCHAR(250) NOT NULL,
-    role       VARCHAR(1) DEFAULT NULL
-);
