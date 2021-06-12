3e2314f2a4c63340a28589ec37371bc3068d4566
vkljucar <valentino.kljucar@gmail.com>
2021-06-12 20:09:33 +0200

    frontend push

--- src/main/java/hr/java/covid_tracker/web/NozarazeniController.java
+++ src/main/java/hr/java/covid_tracker/web/NozarazeniController.java
@@ -1,21 +1,22 @@
 package hr.java.covid_tracker.web;
 
-import hr.java.covid_tracker.novozarazeni.NovozarazeniDTO;
-import hr.java.covid_tracker.novozarazeni.NovozarazeniService;
+import hr.java.covid_tracker.novozarazeni.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("novozarazeni-pregled")
+@RequestMapping("novozarazeni")
 @CrossOrigin(origins = "http://localhost:8080")
-public class NovozarazeniPregledController {
+public class NozarazeniController {
 
     private final NovozarazeniService novozarazeniService;
 
-    public NovozarazeniPregledController(NovozarazeniService novozarazeniService) {
+    public NozarazeniController(NovozarazeniService novozarazeniService) {
         this.novozarazeniService = novozarazeniService;
     }
 
@@ -30,6 +31,24 @@ public class NovozarazeniPregledController {
                 .orElseThrow(
                         () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Nije pronađena ni jedna osoba")
                 );
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public NovozarazeniDTO save(@Valid @RequestBody final NovozarazeniCommand novozarazeniCommand) {
+        return novozarazeniService.save(novozarazeniCommand);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PutMapping("/{id}")
+    public void update(@PathVariable int id, @Valid @RequestBody final NovozarazeniCommand updateNovozarazeniCommand) {
+        novozarazeniService.update(id, updateNovozarazeniCommand);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{oboljeli_id}")
+    public void delete(@PathVariable final int oboljeli_id) {
+        novozarazeniService.deleteById(oboljeli_id);
     }
 
 }
